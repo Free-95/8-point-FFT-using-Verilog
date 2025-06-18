e06ec3024c0d1501268b5892eebf7a814c258ccb
Tanay Shah <ts765147@gmail.com>
2025-06-19 04:21:44 +0530

    clean code. normal floating point working

--- compile_adder_mult.ps1
+++ compile_adder_mult.ps1
@@ -1,16 +1,16 @@
 # Exit immediately if a command fails
 $ErrorActionPreference = "Stop"
 
-iverilog -g2012 -o sim.out tb_posit16_mult.v posit16_mult.v
+iverilog -g2012 -o outputs/sim.out tb_posit16_mult.v posit16_mult.v
 
-vvp sim.out
+vvp outputs/sim.out
 
-iverilog -g2012 -o sim.out tb_posit16_adder.v posit16_adder.v
+iverilog -g2012 -o outputs/sim.out tb_posit16_adder.v posit16_adder.v
 
-vvp sim.out
+vvp outputs/sim.out
 
-iverilog -g2012 -o sim.out butterfly2p.v cpx_mad.v bit_reverse_mapper.v float_adder.v float_multi.v twiddleROM.v fft8_tb.v fft8_fp.v
+iverilog -g2012 -o outputs/sim.out butterfly2p.v cpx_mad.v bit_reverse_mapper.v float_adder.v float_multi.v twiddleROM.v fft8_tb.v fft8_fp.v
 
-vvp sim.out
+vvp outputs/sim.out
 
-Start-Process "gtkwave.exe" "outputs/*"
+Start-Process "gtkwave.exe" "outputs/*.vcd"
